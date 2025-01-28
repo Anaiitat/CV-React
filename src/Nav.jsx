b2f2b4d2a9c60a9c7eb583ffec494a1028c5b812
tatiana <tatiedu84@gmail.com>
2025-01-28 14:00:59 +0100

    Ajout header

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,32 +1,100 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "./App.css";
 
+
+
+
 const Nav = () => {
+  const location = useLocation(); 
+
   return (
-    <header className="bg-stone-900 flex justify-between">
-      <nav>
-        <h1>John Doe</h1>
-        <ul>
-          <li>
-            <Link to="/">Accueil</Link>
-          </li>
-          <li>
-            <Link to="/services">Services</Link>
-          </li>
-          <li>
-            <Link to="/realisation">Realisation</Link>
-          </li>
-          <li>
-            <Link to="/blog">Blog</Link>
-          </li>
-          <li>
-            <Link to="/Contact">Contact</Link>
-          </li>
-          <li>
-            <Link to="/mentions">Mentions</Link>
-          </li>
-        </ul>
+    <header>
+      <nav
+        className="navbar navbar-expand-lg fixed-top fw-semibold text-uppercase"
+        id="mainNav"
+      >
+        <div className="container">
+          <h1 className="navbar-brand m-0  fs-3 text-light">John Doe</h1>
+          <button
+            class="navbar-toggler border-amber-50 text-white border-0"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarResponsive"
+            aria-controls="navbarResponsive"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="fas fa-bars"></span>
+          </button>
+          <div
+            className="collapse  nav-pills navbar-collapse"
+            id="navbarResponsive"
+          >
+            <ul className="navbar-nav  nav-pills ms-auto">
+              <li className=" m-2">
+                <Link
+                  to="/"
+                  className={`nav-link  ${
+                    location.pathname === "/"
+                      ? "active text-light"
+                      : "text-secondary"
+                  }`}
+                >
+                  Accueil
+                </Link>
+              </li>
+              <li className=" m-2">
+                <Link
+                  to="/services"
+                  className={`nav-link ${
+                    location.pathname === "/services"
+                      ? "active text-light"
+                      : "text-secondary"
+                  }`}
+                >
+                  Services
+                </Link>
+              </li>
+              <li className=" m-2">
+                <Link
+                  to="/realisation"
+                  className={`nav-link ${
+                    location.pathname === "/realisation"
+                      ? "active text-light"
+                      : "text-secondary"
+                  }`}
+                >
+                  Réalisation
+                </Link>
+              </li>
+              <li className=" m-2">
+                <Link
+                  to="/blog"
+                  className={`nav-link ${
+                    location.pathname === "/blog"
+                      ? "active text-light"
+                      : "text-secondary"
+                  }`}
+                >
+                  Blog
+                </Link>
+              </li>
+              <li className=" m-2">
+                <Link
+                  to="/Contact"
+                  className={`nav-link ${
+                    location.pathname === "/Contact"
+                      ? "active text-light "
+                      : "text-secondary"
+                  }`}
+                >
+                  Me Contacter
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
       </nav>
     </header>
   );
