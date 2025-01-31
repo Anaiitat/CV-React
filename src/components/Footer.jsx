099b860497dc11d4a74dcf321bd71876d44c4c2e
Anaiitat <73583249+Anaiitat@users.noreply.github.com>
2025-01-31 09:37:43 +0100

    Finition footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
 
     return (
       <footer className="pt-5">
-        <div className="container-fluid px-5">
+        <div className="container-fluid ">
           <div className="row px-5 ">
             <div className="col-lg-3 col-md-6 ">
               <h5>John Doe</h5>
@@ -19,13 +19,28 @@ const Footer = () => {
                 <p>Téléphone : 06 20 30 40 50</p>
               </address>
               <div>
-                <a className="p-2 ">
+                <a
+                  href="https://github.com/github-john-doe"
+                  rel="nofollow"
+                  target="blank"
+                  className="p-2 "
+                >
                   <i class="fa-brands fa-github fa-2xl"></i>
                 </a>
-                <a className="p-2">
+                <a
+                  href="https://x.com/?lang=fr"
+                  rel="nofollow"
+                  target="blank"
+                  className="p-2"
+                >
                   <i class="fa-brands fa-twitter fa-2xl"></i>
                 </a>
-                <a className="p-2">
+                <a
+                  href="https://www.linkedin.com/feed/"
+                  rel="nofollow"
+                  target="blank"
+                  className="p-2"
+                >
                   <i class="fa-brands fa-linkedin fa-2xl"></i>
                 </a>
               </div>
@@ -35,35 +50,98 @@ const Footer = () => {
               <ul className="ps-1">
                 <li>
                   <i class="fa-solid fa-chevron-right i-chevron"></i>
-                  <Link to="/accueil" className="link"> Accueil</Link>
+                  <Link to="/" className="link">
+                    {" "}
+                    Accueil
+                  </Link>
                 </li>
                 <li>
                   <i class="fa-solid fa-chevron-right i-chevron"></i>
-                  <Link to="/propos" className="link"> A propos</Link>
+                  <Link to="/#apropos" className="link">
+                    {" "}
+                    A propos
+                  </Link>
                 </li>
                 <li>
                   <i class="fa-solid fa-chevron-right i-chevron"></i>
-                  <Link to="/services" className="link"> Services</Link>
+                  <Link to="/services" className="link">
+                    {" "}
+                    Services
+                  </Link>
                 </li>
                 <li>
                   <i class="fa-solid fa-chevron-right i-chevron"></i>
-                  <Link to="/contact" className="link"> Me contacter</Link>
+                  <Link to="/contact" className="link">
+                    {" "}
+                    Me contacter
+                  </Link>
                 </li>
                 <li>
                   <i class="fa-solid fa-chevron-right i-chevron"></i>
-                  <Link to="/mentions" className="link"> Mentions légale</Link>
+                  <Link to="/mentions" className="link">
+                    {" "}
+                    Mentions légale
+                  </Link>
                 </li>
               </ul>
             </div>
-            <div className="col-lg-3 col-md-6">
+            <div className="col-lg-3 col-md-6 ">
               <h5>Mes dernières réalisations</h5>
-              
+              <ul className="ps-1">
+                <li>
+                  <i class="fa-solid fa-chevron-right i-chevron"></i>
+                  <Link to="/realisation" className="link">
+                    {" "}
+                    Fresh food
+                  </Link>
+                </li>
+                <li>
+                  <i class="fa-solid fa-chevron-right i-chevron"></i>
+                  <Link to="/realisation" className="link">
+                    {" "}
+                    Restaurant Akira
+                  </Link>
+                </li>
+                <li>
+                  <i class="fa-solid fa-chevron-right i-chevron"></i>
+                  <Link to="/realisation" className="link">
+                    {" "}
+                    Espace bien-être
+                  </Link>
+                </li>
+              </ul>
             </div>
-            <div className="col-lg-3 col-md-6">
+            <div className="col-lg-3 col-md-6 ">
               <h5>Mes derniers articles</h5>
+              <ul className="ps-1">
+                <li>
+                  <i class="fa-solid fa-chevron-right i-chevron"></i>
+                  <Link to="/blog" className="link">
+                    {" "}
+                    Coder son site en HTML/CSS
+                  </Link>
+                </li>
+                <li>
+                  <i class="fa-solid fa-chevron-right i-chevron"></i>
+                  <Link to="/blog" className="link">
+                    {" "}
+                    Vendre ses produits sur le web
+                  </Link>
+                </li>
+                <li>
+                  <i class="fa-solid fa-chevron-right i-chevron"></i>
+                  <Link to="/blog" className="link">
+                    {" "}
+                    Se positionner sur Google
+                  </Link>
+                </li>
+              </ul>
             </div>
           </div>
         </div>
+        <p className="bg-dark m-0 text-center text-white mt-4 p-3">
+          <i class="fa-regular fa-copyright i-copyright"></i> Designed by John Doe
+        </p>
       </footer>
     );
 
