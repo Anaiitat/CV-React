285ea1816efcc1fc9f965933e42488ebc34fc249
Anaiitat <73583249+Anaiitat@users.noreply.github.com>
2025-01-31 13:43:19 +0100

    Finition accordéon

--- src/pages/Mentions.jsx
+++ src/pages/Mentions.jsx
@@ -1,18 +1,19 @@
 import React from "react";
+import "../css/Mentions.css"
 
 const Mentions = () => {
   return (
     <main>
-      <div className="container text-center py-4">
-        <h1 className="text-uppercase">Mentions légales</h1>
-        <div className="custom-border m-auto"></div>
-      </div>
       <div className="container">
-        <div className="accordion rounded border" id="accordionExample">
-          <div className="acordion-item">
-            <h2 className="accordion-header" id="headingOne">
+        <div className=" text-center py-4">
+          <h1 className="text-uppercase">Mentions légales</h1>
+          <div className="custom-border m-auto"></div>
+        </div>
+        <div class="accordion py-4" id="accordionExample">
+          <div class="accordion-item">
+            <h2 class="accordion-header " id="headingOne">
               <button
-                className="accordion-button"
+                class="accordion-button collapsed "
                 type="button"
                 data-bs-toggle="collapse"
                 data-bs-target="#collapseOne"
@@ -24,30 +25,38 @@ const Mentions = () => {
             </h2>
             <div
               id="collapseOne"
-              class="accordion-collapse collapse show "
+              class="accordion-collapse collapse "
               aria-labelledby="headingOne"
               data-bs-parent="#accordionExample"
             >
-              <div className="accordion-body">
-                <h5>John Doe</h5>
-                <address>
+              <div class="accordion-body">
+                <h2>John Doe</h2>
+                <address className="h-auto mt-2">
                   <p className="m-0">
-                    40 Rue Laure Diebold, <br />
-                    69009 Lyon, France
+                    <i class="fa-solid fa-location-dot"></i> 40 Rue Laure
+                    Diebold
+                    <br /> 69009 Lyon, France
                   </p>
-                  <p>Téléphone : 06 20 30 40 50</p>
+                  <a target="blank" href="0620304050" className="m-0">
+                    <i className=" fa-solid fa-mobile-button"></i> 06 20 30 40
+                    50
+                    <br />
+                  </a>
+                  <a target="blank" href="mailto:john.doe@gmail.com">
+                    <i class=" fa-solid fa-envelope"></i> john.doe@gmail.com
+                  </a>
                 </address>
               </div>
             </div>
           </div>
-          <div className="acordion-item">
-            <h2 className="accordion-header" id="headingTwo">
+          <div class="accordion-item">
+            <h2 class="accordion-header" id="headingTwo">
               <button
-                className="accordion-button"
+                class="accordion-button collapsed"
                 type="button"
                 data-bs-toggle="collapse"
                 data-bs-target="#collapseTwo"
-                aria-expanded="true"
+                aria-expanded="false"
                 aria-controls="collapseTwo"
               >
                 Hébergeur
@@ -55,28 +64,32 @@ const Mentions = () => {
             </h2>
             <div
               id="collapseTwo"
-              class="accordion-collapse collapse accordion-body"
+              class="accordion-collapse collapse"
               aria-labelledby="headingTwo"
               data-bs-parent="#accordionExample"
             >
-              <h5>John Doe</h5>
-              <address>
-                <p className="m-0">
-                  40 Rue Laure Diebold, <br />
-                  69009 Lyon, France
-                </p>
-                <p>Téléphone : 06 20 30 40 50</p>
-              </address>
+              <div class="accordion-body">
+                <h2>Always Data</h2>
+                <address className="m-0">
+                  <p>
+                    91 rue du Faubourg Saint Honoré <br />
+                    75008 Paris
+                  </p>
+                  <a href="www.alwaysdata.com">
+                    <i class="fa-solid fa-globe"></i> www.alwaysdata.com
+                  </a>
+                </address>
+              </div>
             </div>
           </div>
-          <div className="acordion-item">
-            <h2 className="accordion-header" id="headingThree">
+          <div class="accordion-item">
+            <h2 class="accordion-header" id="headingThree">
               <button
-                className="accordion-button"
+                class="accordion-button collapsed"
                 type="button"
                 data-bs-toggle="collapse"
                 data-bs-target="#collapseThree"
-                aria-expanded="true"
+                aria-expanded="false"
                 aria-controls="collapseThree"
               >
                 Crédits
@@ -84,18 +97,19 @@ const Mentions = () => {
             </h2>
             <div
               id="collapseThree"
-              class="accordion-collapse collapse  accordion-body"
-              aria-labelledby="headingOne"
+              class="accordion-collapse collapse"
+              aria-labelledby="headingThree"
               data-bs-parent="#accordionExample"
             >
-              <h5>John Doe</h5>
-              <address>
-                <p className="m-0">
-                  40 Rue Laure Diebold, <br />
-                  69009 Lyon, France
+              <div class="accordion-body">
+                <h2>Crédits</h2>
+                <p>Site développé par John Doe, étudiant du CEF.</p>
+                <p>
+                  Les images libres de droit sont issues du site <a href="https://pixabay.com/fr/" rel="nofollow" target="blank"
+                  > Pixabay
+                  </a>.
                 </p>
-                <p>Téléphone : 06 20 30 40 50</p>
-              </address>
+              </div>
             </div>
           </div>
         </div>
