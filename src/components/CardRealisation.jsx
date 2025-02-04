34ba9ebc7ea628b74fd8d4cdfb5faec066bde4bd
Anaiitat <73583249+Anaiitat@users.noreply.github.com>
2025-02-04 21:59:50 +0100

    rajout des alt dans les images de realisation + finition page blog

--- src/components/CardRealisation.jsx
+++ src/components/CardRealisation.jsx
@@ -7,7 +7,7 @@ import Espace from "../img/portfolio/espace-bien-etre.jpg";
 function CardRealisations(props){
   return (
     <article className=" card col-md-6 col-lg-3 p-0 m-3 ">
-      <img src={props.img} className="card-img-top" />
+      <img src={props.img} alt={props.alt} className="card-img-top" />
       <div className="card-body ">
         <h3 className="card-title">{props.titre}</h3>
         <p>{props.description}</p>
@@ -23,9 +23,9 @@ function CardRealisations(props){
 export default function CardRealisation(){
   return (
     <div className="row my-4 text-center align-items-start justify-content-center">
-      <CardRealisations img={FreshFood} titre="Fresh food" description="Réalisation d'un site avec commande en ligne." tech="PHP et MySQL" />
-      <CardRealisations img={Restaurant} titre="Restaurant Akira" description="Réalisation d'un site vitrine." tech="WordPress" />
-      <CardRealisations img={Espace} titre="Espace bien-être" description="Réalisation d'un site vitrine pour un praticien de bien-être." tech="HTML/CSS" />
+      <CardRealisations alt="Panier de fruits" img={FreshFood} titre="Fresh food" description="Réalisation d'un site avec commande en ligne." tech="PHP et MySQL" />
+      <CardRealisations alt="Menu Sushis" img={Restaurant} titre="Restaurant Akira" description="Réalisation d'un site vitrine." tech="WordPress" />
+      <CardRealisations alt="Espace bien-être" img={Espace} titre="Espace bien-être" description="Réalisation d'un site vitrine pour un praticien de bien-être." tech="HTML/CSS" />
     </div>
   );
 }
