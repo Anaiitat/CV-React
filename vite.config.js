1d74d8c955de9f40329e31c4c9580973bfe29fc2
Anaiitat <73583249+Anaiitat@users.noreply.github.com>
2025-02-05 19:09:02 +0100

    modife vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    allowedHosts: ["xmwqxc-5173.csb.app"], // Autoriser CodeSandbox
+    host: true, // Autorise les connexions externes
+}})
