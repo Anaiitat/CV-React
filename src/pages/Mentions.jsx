32a9d089a691a0a77203693ed01398b1de748a8b
Anaiitat <73583249+Anaiitat@users.noreply.github.com>
2025-02-05 14:46:30 +0100

    Finition page mentions

--- src/pages/Mentions.jsx
+++ src/pages/Mentions.jsx
@@ -1,5 +1,7 @@
 import React from "react";
-import "../css/Mentions.css"
+import "../css/Mentions.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLocationDot, faMobileButton, faEnvelope, faGlobe } from "@fortawesome/free-solid-svg-icons";
 
 const Mentions = () => {
   return (
@@ -9,105 +11,78 @@ const Mentions = () => {
           <h1 className="text-uppercase">Mentions légales</h1>
           <div className="custom-border m-auto"></div>
         </div>
-        <div class="accordion py-4" id="accordionExample">
-          <div class="accordion-item">
-            <h2 class="accordion-header " id="headingOne">
-              <button
-                class="accordion-button collapsed "
-                type="button"
-                data-bs-toggle="collapse"
-                data-bs-target="#collapseOne"
-                aria-expanded="true"
-                aria-controls="collapseOne"
-              >
+        <div className="accordion py-4" id="accordionExample">
+          <div className="accordion-item">
+            <h2 className="accordion-header " id="headingOne">
+              <button className="accordion-button collapsed " type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
                 Editeur du site
               </button>
             </h2>
-            <div
-              id="collapseOne"
-              class="accordion-collapse collapse "
-              aria-labelledby="headingOne"
-              data-bs-parent="#accordionExample"
-            >
-              <div class="accordion-body">
+            <div id="collapseOne" className="accordion-collapse collapse " aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+              <div className="accordion-body">
                 <h2>John Doe</h2>
                 <address className="h-auto mt-2">
                   <p className="m-0">
-                    <i class="fa-solid fa-location-dot"></i> 40 Rue Laure
-                    Diebold
+                    <FontAwesomeIcon icon={faLocationDot} /> 40 Rue Laure Diebold
                     <br /> 69009 Lyon, France
                   </p>
-                  <a target="blank" href="0620304050" className="m-0">
-                    <i className=" fa-solid fa-mobile-button"></i> 06 20 30 40
-                    50
-                    <br />
-                  </a>
-                  <a target="blank" href="mailto:john.doe@gmail.com">
-                    <i class=" fa-solid fa-envelope"></i> john.doe@gmail.com
-                  </a>
+                  <p className="m-0">
+                    <FontAwesomeIcon icon={faMobileButton} />
+                    <a target="blank" href="0620304050">
+                      {" "}
+                      06 20 30 40 50
+                    </a>
+                  </p>
+                  <p>
+                    <FontAwesomeIcon icon={faEnvelope} />
+                    <a target="blank" href="mailto:john.doe@gmail.com">
+                      {" "}
+                      john.doe@gmail.com
+                    </a>
+                  </p>
                 </address>
               </div>
             </div>
           </div>
-          <div class="accordion-item">
-            <h2 class="accordion-header" id="headingTwo">
-              <button
-                class="accordion-button collapsed"
-                type="button"
-                data-bs-toggle="collapse"
-                data-bs-target="#collapseTwo"
-                aria-expanded="false"
-                aria-controls="collapseTwo"
-              >
+          <div className="accordion-item">
+            <h2 className="accordion-header" id="headingTwo">
+              <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
                 Hébergeur
               </button>
             </h2>
-            <div
-              id="collapseTwo"
-              class="accordion-collapse collapse"
-              aria-labelledby="headingTwo"
-              data-bs-parent="#accordionExample"
-            >
-              <div class="accordion-body">
+            <div id="collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+              <div className="accordion-body">
                 <h2>Always Data</h2>
                 <address className="m-0">
                   <p>
                     91 rue du Faubourg Saint Honoré <br />
                     75008 Paris
                   </p>
-                  <a href="www.alwaysdata.com">
-                    <i class="fa-solid fa-globe"></i> www.alwaysdata.com
-                  </a>
+                  <p className="m-0">
+                    <FontAwesomeIcon icon={faGlobe} />
+                    <a href="www.alwaysdata.com"> www.alwaysdata.com</a>
+                  </p>
                 </address>
               </div>
             </div>
           </div>
-          <div class="accordion-item">
-            <h2 class="accordion-header" id="headingThree">
-              <button
-                class="accordion-button collapsed"
-                type="button"
-                data-bs-toggle="collapse"
-                data-bs-target="#collapseThree"
-                aria-expanded="false"
-                aria-controls="collapseThree"
-              >
+          <div className="accordion-item">
+            <h2 className="accordion-header" id="headingThree">
+              <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
                 Crédits
               </button>
             </h2>
-            <div
-              id="collapseThree"
-              class="accordion-collapse collapse"
-              aria-labelledby="headingThree"
-              data-bs-parent="#accordionExample"
-            >
-              <div class="accordion-body">
+            <div id="collapseThree" className="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+              <div className="accordion-body">
                 <h2>Crédits</h2>
                 <p>Site développé par John Doe, étudiant du CEF.</p>
                 <p>
-                  Les images libres de droit sont issues du site <a href="https://pixabay.com/fr/" rel="nofollow" target="blank"
-                  > Pixabay
-                  </a>.
+                  Les images libres de droit sont issues du site{" "}
+                  <a href="https://pixabay.com/fr/" rel="nofollow" target="blank">
+                    {" "}
+                    Pixabay
+                  </a>
+                  .
                 </p>
               </div>
             </div>
